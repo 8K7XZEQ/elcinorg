b74fabc150ccaab40abe743eaed8419e52794d42
Elchin <elcinvm6@gmail.com>
2021-11-14 12:06:46 +0400

    Names

---
+++ orgstructure/src/pages/structure/cedvel.js
@@ -0,0 +1,180 @@
+export const employees = [{
+    "id": 1,
+    "parent_id": -1,
+    "name": "John Heart",
+    "status": true,
+    }, {
+    "id": 2,
+    "parent_id": 1,
+    "name": "Samantha Bright",
+    "status": true,
+    }, {
+    "id": 3,
+    "parent_id": 1,
+    "name": "Arthur Miller",
+    "status": true,
+    
+}, {
+    "id": 4,
+    "parent_id": 1,
+    "name": "Robert Reagan",
+    "status": true,
+    
+}, {
+    "id": 5,
+    "parent_id": 1,
+    "name": "Greta Sims",
+   "status": true,
+   
+}, {
+    "id": 6,
+    "parent_id": 3,
+    "name": "Brett Wade",
+    "status": true,
+    
+}, {
+    "id": 7,
+    "parent_id": 5,
+    "name": "Sandra Johnson",
+    "status": true,
+    
+}, {
+    "id": 8,
+    "parent_id": 4,
+    "name": "Ed Holmes",
+    "status": true,
+    
+}, {
+    "id": 9,
+    "parent_id": 3,
+    "name": "Barb Banks",
+    "status": true,
+    
+}, {
+    "id": 10,
+    "parent_id": 2,
+    "name": "Kevin Carter",
+    "status": true,
+    
+}, {
+    "id": 11,
+    "parent_id": 5,
+    "name": "Cindy Stanwick",
+    "status": true,
+    
+}, {
+    "id": 12,
+    "parent_id": 8,
+    "name": "Sammy Hill",
+   "status": true,
+
+   
+}, {
+    "id": 13,
+    "parent_id": 10,
+    "name": "Davey Jones",
+    "status": true,
+    
+}, {
+    "id": 14,
+    "parent_id": 10,
+    "name": "Victor Norris",
+    "status": true,
+    
+}, {
+    "id": 15,
+    "parent_id": 10,
+    "name": "Mary Stern",
+    "status": true,
+    
+}, {
+    "id": 16,
+    "parent_id": 10,
+    "name": "Robin Cosworth",
+    "status": true,
+    
+}, {
+    "id": 17,
+    "parent_id": 9,
+    "name": "Kelly Rodriguez",
+    "status": true,
+    
+}, {
+    "id": 18,
+    "parent_id": 9,
+    "name": "James Anderson",
+    "status": true,
+    
+}, {
+    "id": 19,
+    "parent_id": 9,
+    "name": "Antony Remmen",
+    "status": true,
+    
+}, {
+    "id": 20,
+    "parent_id": 8,
+    "name": "Olivia Peyton",
+   "status": true,
+    
+}, {
+    "id": 21,
+    "parent_id": 6,
+    "name": "Taylor Riley",
+    "status": true,
+    
+}, {
+    "id": 22,
+    "parent_id": 6,
+    "name": "Amelia Harper",
+    "status": true,
+    
+}, {
+    "id": 23,
+    "parent_id": 6,
+    "name": "Wally Hobbs",
+    "status": true,
+    
+}, {
+    "id": 24,
+    "parent_id": 6,
+    "name": "Brad Jameson",
+    "status": true,
+    
+}, {
+    "id": 25,
+    "parent_id": 6,
+    "name": "Karen Goodson",
+    "status": true,
+    
+}, {
+    "id": 26,
+    "parent_id": 5,
+    "name": "Marcus Orbison",
+    "status": true,
+    
+}, {
+    "id": 27,
+    "parent_id": 5,
+    "name": "Sandy Bright",
+    "status": true,
+
+}, {
+    "id": 28,
+    "parent_id": 6,
+    "name": "Morgan Kennedy",
+   "status": true,
+    
+}, {
+    "id": 29,
+    "parent_id": 28,
+    "name": "Violet Bailey",
+    "status": false,
+    
+}, {
+    "id": 30,
+    "parent_id": 5,
+    "name": "Ken Samuelson",
+    "status": false,
+    
+}];
